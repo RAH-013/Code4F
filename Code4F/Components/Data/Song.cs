ccab475e7db3e16aa6178bf22bcbe48dd163fcfe
RAH-013 <ranguiano7@ucol.mx>
2025-04-06 19:53:54 -0600

    Agregar archivos de proyecto.

---
+++ Code4F/Components/Data/Song.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Code4F.Components.Data
+{
+    public class Song
+    {
+        [Required(ErrorMessage = "El campo \"Nombre\" no puede quedar vacio.")]
+        public string Name { get; set; } = "";
+
+        public bool State { get; set; } = true;
+    }
+}
---
+++ Code4F/Components/Data/Task.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Code4F.Components.Data
+{
+    public class ToDoTask
+    {
+        [Required(ErrorMessage = "El campo \"Nombre\" no puede quedar vacio.")]
+        public string? Name { get; set; }
+        [Required(ErrorMessage = "El campo \"Descripción\" no puede quedar vacio.")]
+        public string? Description { get; set; }
+    }
+}
