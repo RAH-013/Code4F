c3ba0f67dee3e302af9c37bb205dcee660231a1c
RAH-013 <ranguiano7@ucol.mx>
2025-04-08 11:15:44 -0600

    Examen

---
+++ Code4F/Components/Data/Product.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Code4F.Components.Data
+{
+    public class Product
+    {
+        [Required(ErrorMessage = "El campo \"Inventario\" es obligatorio.")]
+        [Range(1, int.MaxValue, ErrorMessage = "El campo \"Inventario\" debe ser un entero positivo mayor a 0.")]
+        public int Inv { get; set; } = 0;
+        [Required(ErrorMessage = "El campo \"Nombre\" no puede quedar vacio.")]
+        public string Name { get; set; } = string.Empty;
+        [Required(ErrorMessage = "El campo \"Departamento\" no puede quedar sin seleccionar.")]
+        public string Dep { get; set; } = string.Empty;
+    }
+}
